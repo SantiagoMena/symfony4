4ededf38e4a98313d24a9e76fdaaf03853c056a8
Santiago Mena <santiagomenape@gmail.com>
2022-06-07 00:01:10 -0300

    Assets desde un CDN

--- symfony/src/Controller/ComponentController.php
+++ symfony/src/Controller/ComponentController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
 use App\AssetVersionStrategy\DateVersionStrategy;
 use Symfony\Component\Asset\PathPackage;
+use Symfony\Component\Asset\UrlPackage;
 
 class ComponentController extends AbstractController
 {
@@ -25,6 +26,11 @@ class ComponentController extends AbstractController
         // Paquete de activos agrupados por carpetas
         $pathPackage = new PathPackage('css', new EmptyVersionStrategy());
 
+        $urlPackage = new UrlPackage(
+            '//localhost:83/',
+            new StaticVersionStrategy('v1', '%2$s/%1$s')
+        );
+
         return new Response
         ("
             <html>
@@ -33,11 +39,13 @@ class ComponentController extends AbstractController
                     <link rel=\"stylesheet\" href=\"". $packageV1->getUrl("/css/test_assets.css") . "\">
                     <link rel=\"stylesheet\" href=\"". $packageDateVersion->getUrl("/css/test_assets.css") . "\">
                     <link rel=\"stylesheet\" href=\"". $pathPackage->getUrl("test_assets_package.css") . "\">
+                    <link rel=\"stylesheet\" href=\"". $urlPackage->getUrl("css/test_assets_cdn.css") . "\">
                 </head>
                 <body>
                     <p>Test Assets</p>
                     <b>Test Assets Versión</b>
                     <a>Test Assets Package</a>
+                    <h1>Test Assets Package CDN</h1>
                 </body>
             </html>
         ");
