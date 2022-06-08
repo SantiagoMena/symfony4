ecbca1dc495c5ba91b310896f6764a04c95135b3
Santiago Mena <santiagomenape@gmail.com>
2022-06-08 18:27:20 -0300

    Ejemplos de rutas: GET, POST, PUT, DELETE

--- symfony/src/Controller/RoutesController.php
+++ symfony/src/Controller/RoutesController.php
@@ -9,12 +9,57 @@ use Symfony\Component\Routing\Annotation\Route;
 class RoutesController extends AbstractController
 {
     /**
-     * @Route("/routes/ejemplo1", name="app_routes_ejemplo1")
+     * @Route("/routes/ejemplo", name="app_routes_ejemplo")
+     * @return Response
      */
-    public function ejemplo1(): Response
+    public function ejemplo(): Response
     {
         return $this->render('routes/index.html.twig', [
-            'title' => 'Ejemplo de rutas #1',
+            'title' => 'Ejemplo de rutas',
+        ]);
+    }
+
+    /**
+     * @Route("/routes/ejemploGet", name="app_routes_ejemplo_get", methods={"GET", "HEAD"})
+     * @return Response
+     */
+    public function ejemploGet(): Response
+    {
+        return $this->render('routes/index.html.twig', [
+            'title' => 'Ejemplo de rutas GET',
+        ]);
+    }
+
+    /**
+     * @Route("/routes/ejemploPost", name="app_routes_ejemplo_post", methods={"POST"})
+     * @return Response
+     */
+    public function ejemploPost(): Response
+    {
+        return $this->render('routes/index.html.twig', [
+            'title' => 'Ejemplo de rutas POST',
+        ]);
+    }
+
+    /**
+     * @Route("/routes/ejemploPut", name="app_routes_ejemplo_put", methods={"PUT"})
+     * @return Response
+     */
+    public function ejemploPut(): Response
+    {
+        return $this->render('routes/index.html.twig', [
+            'title' => 'Ejemplo de rutas PUT',
+        ]);
+    }
+
+    /**
+     * @route("/routes/ejemploDelete", name="app_routes_ejemplo_delete", methods={"DELETE"})
+     * @return Response
+     */
+    public function ejemploDelete(): Response
+    {
+        return $this->render('routes/index.html.twig', [
+            'title' => 'Ejemplo de rutas DELETE'
         ]);
     }
 }
--- symfony/tests/Application/Controller/RoutesControllerTest.php
+++ symfony/tests/Application/Controller/RoutesControllerTest.php
@@ -6,12 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RoutesControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testEjemplo(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/routes/ejemplo1');
+        $crawler = $client->request('GET', '/routes/ejemplo');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Ejemplo de rutas #1');
+        $this->assertSelectorTextContains('h1', 'Ejemplo de rutas');
+    }
+
+    /** @dataProvider dataMethods */
+    public function testEjemplosMethod($route, $method): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request($method, '/routes/'. $route);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Ejemplo de rutas ' . $method);
+    }
+
+    public function dataMethods()
+    {
+        yield ['ejemploGet', 'GET'];
+        yield ['ejemploPost', 'POST'];
+        yield ['ejemploPut', 'PUT'];
+        yield ['ejemploDelete', 'DELETE'];
     }
 }
