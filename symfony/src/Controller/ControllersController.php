c2725937b52c500bcd79485ccfe63f00c8b525f8
Santiago Mena <santiagomenape@gmail.com>
2022-07-27 15:28:08 -0300

    Controlador: Redirect

---
+++ symfony/src/Controller/ControllersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class ControllersController extends AbstractController
+{
+    /**
+     * @Route("/controllers/index", name="app_controllers_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('controllers/index.html.twig', [
+            'title' => 'Index'
+        ]);
+    }
+
+    /**
+     * @Route("/controllers/redirect", name="app_controllers_redireccion_ruta")
+     */
+    public function redireccionRuta(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_controllers_index', [], 301);
+    }
+
+    /**
+     * @Route("/controllers/redirect-externa", name="app_controllers_redireccion_externa")
+     */
+    public function redireccionExterna(): RedirectResponse
+    {
+        return $this->redirect('https://symfony.com');
+    }
+}
---
+++ symfony/tests/Application/Controller/ControllersControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Application\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ControllersControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/controllers/index');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Index');
+    }
+
+    public function testRedirect(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/controllers/redirect');
+
+        $this->assertResponseRedirects();
+    }
+
+    public function testRedirectExterna(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/controllers/redirect-externa');
+
+        $this->assertResponseRedirects();
+    }
+}
